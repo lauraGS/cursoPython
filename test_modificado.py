99db6488609faa5326f3d64a3f1266ababc6f3a2
Laura Gil <laura.gil@experis.es>
2021-05-24 19:52:04 +0200

    segundo ejercicio

---
+++ test_modificado.py
@@ -0,0 +1,12 @@
+x = 2
+texto=''
+if x < 0:
+    texto = 'Negative changed to zero'
+elif x == 0:
+    texto = 'Zero'
+elif x == 1:
+    texto = 'Single'
+else:
+    texto = 'Terminado'
+
+print(texto)
