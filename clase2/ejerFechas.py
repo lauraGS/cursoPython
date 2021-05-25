8792369f061bf460ef0eb473457ef898a5a0b47c
Laura Gil <laura.gil@experis.es>
2021-05-25 19:52:44 +0200

    clase2/ejerFechas.py

---
+++ clase2/ejerFechas.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+today = datetime.today()
+
+print("El día actual es {}".format(today.day))
+print("El mes actual es {}".format(today.month))
+print("El año actual es {}".format(today.year))
+
+print("La hora actual es {}".format(today.hour))
+print("El minuto actual es {}".format(today.minute))
+print("El segundo actual es {}".format(today.second))
