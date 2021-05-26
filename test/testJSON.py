ab5c5a5e78035bbc5c1990da5436f16781209009
Laura Gil <laura.gil@experis.es>
2021-05-26 18:45:35 +0200

    tests

---
+++ test/testJSON.py
@@ -0,0 +1,12 @@
+import json
+import unittest
+
+class TestPrueba(unittest.TestCase):
+    def test_method1(self):
+        student = {"101":{"class":'V', "Name":'Rohit',  "Roll_no":7},
+                    "102":{"class":'V', "Name":'David',  "Roll_no":8},
+                    "103":{"class":'V', "Name":'Samiya', "Roll_no":12}}
+        self.assertEqual(student["101"], {"class":'V', "Name":'Rohit',  "Roll_no":7})
+        self.assertEqual(list(student.keys()), ["101", "102", "103"])
+        self.assertEqual(list(student["101"].keys()), ["class", "Name", "Roll_no"])
+
---
+++ test/testLista.py
@@ -0,0 +1,19 @@
+import json
+import unittest
+
+class TestListas(unittest.TestCase):
+    def test_method1(self):
+        lista = ["apple", "banana", "cherry", "orange", "kiwi"]
+        milista2 = sorted(lista, key=str.lower)
+        self.assertEqual((milista2), ['apple', 'banana', 'cherry', 'kiwi', 'orange'])
+        lista.insert(2, "mango")
+        self.assertEqual(lista, ['apple', 'banana', 'mango', 'cherry', 'orange', 'kiwi'])
+        lista.remove("banana")
+        lista.remove("cherry")
+        self.assertEqual(lista,['apple', 'mango', 'orange', 'kiwi'])
+        lista.extend(["banana", "cherry"])
+        self.assertEqual(lista,['apple', 'mango', 'orange', 'kiwi', 'banana', 'cherry'])
+        del lista[0]
+        lista.pop(0)
+        self.assertEqual(lista, ['orange', 'kiwi', 'banana', 'cherry'])
+        self.assertEqual(lista[-1], 'cherry')
